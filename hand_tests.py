fe0af0f442e83ff7614312fd327045333c09a970
USER-PC2 <kirshner21102010@gmail.com>
2023-12-15 18:01:15 +0300

    dfgdrgxdfgrx

---
+++ hand_tests.py
@@ -0,0 +1,10 @@
+from main import salary,hello_who
+# Тесты с помощью if
+ salary !=20:
+
+assert hello_who('Max') =='Hello,Max':
+
+
+assert hello_who('Leo') !='Hello,Leo':
+
+
---
+++ simple_autotest.py
@@ -0,0 +1,8 @@
+#Тесты через ssert
+from functions import salary,hello_who
+
+assert hello_who('Max')=='Hello,Max'
+assert 3==3
+assert 4==4
+assert 2==4
+print(4,5,3,5)
